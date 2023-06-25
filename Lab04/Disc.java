a318833c434aad3e2554f2d48ba66cf8cb00120b
hexadex404 <93644942+hexadex404@users.noreply.github.com>
2023-06-26 01:30:58 +0700

    Upload all completed lab assignments up to week 4

---
+++ Lab04/Disc.java
@@ -0,0 +1,16 @@
+public class Disc extends Media{
+    private int length;
+    private String director;
+    
+    public int getLength(){
+        return length;
+    }
+
+    public String getDirector(){
+        return director;
+    }
+
+    public Disc(){
+        super();
+    }
+}
